b5f9e4b7488b0bc240634549dbdb39d839b8eb1c
thy-irl <thy.nguyen@katalon.com>
2025-04-19 17:07:56 +0700

    added new TC

---
+++ Scripts/Custom-keyword samples/Checkout failed/Script1744547703246.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+CustomKeywords.'sample.Login.loginIntoApplicationWithGlobalVariable'()
+
+WebUI.waitForElementPresent(findTestObject('Pages/Shop page/lnkShop'), GlobalVariable.waitPresentTimeout)
+
+WebUI.click(findTestObject('Pages/Shop page/lnkShop'))
+
+CustomKeywords.'sample.Shop.addToCartWithGlobalVariable'()
+
+CustomKeywords.'sample.Checkout.CheckoutShop'(
+	'Mark', 'Emoji', 'FunCo',
+	'Vietnam',
+	'123 tEST', 'EmojiCity', '12345', '1234567890')
+
+CustomKeywords.'sample.Login.logoutFromApplication'()
+
+WebUI.closeBrowser()
+
+
